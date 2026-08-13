c0506e20da67a010cf66cf7d9eb0f4d23006f900
AdityaTuljapurkar <adityatuljapurkar90018@gmail.com>
2026-08-13 22:03:31 +0000

    add login feature

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': 'dashboard_DSA_MENTORE',
         'USER': 'aditya',
         'PASSWORD': 'aditya',
-        'HOST': 'database',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ConnectionCheck from './pages/ConnectionCheck'
 import Homepage from './pages/homepage'
-import Login from './pages/Login'
 function App() {
 
   return (
@@ -9,7 +8,6 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage />}/>
       <Route path='check-django' element={<ConnectionCheck/>}/>
-      <Route path='login' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   )
