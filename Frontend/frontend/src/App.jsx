36a3bda617bc44c7acf85833904176defee577fa
AdityaTuljapurkar <adityatuljapurkar90018@gmail.com>
2026-08-17 15:21:21 +0000

    Add login and dashboard pages

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ConnectionCheck from './pages/ConnectionCheck'
 import Homepage from './pages/homepage'
+import Login from './pages/Login';
+import Dashboard from './Dashboard';
 function App() {
 
   return (
@@ -8,8 +10,10 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage />}/>
       <Route path='check-django' element={<ConnectionCheck/>}/>
+      <Route path='/login' element={<Login></Login>} ></Route>
+      <Route path='/dashboard'element={<Dashboard/>}/>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>  
   )
 }
 
---
+++ Frontend/frontend/src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import {useNavigate } from "react-router-dom";
+import axios from "axios" ; 
+
+export default function Login(){
+    const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);  
+  const navigate = useNavigate() ; 
+  
+  async function handleSubmit(event){
+    event.preventDefault()
+    setError("")
+    setLoading(true)
+
+    try {
+        const response =  await axios.post(
+            "http://127.0.0.1:8000/api/login/", 
+            {username , password}
+        ) ; 
+        localStorage.setItem("accessToken",response.data.access)
+    localStorage.setItem("refreshToken", response.data.refresh)
+        navigate("/dashboard")
+    } catch(err){
+        if (err.response?.status == 401){
+            setError("Invalid username or password") , 
+        }
+        else {
+            setError("could not connet to the server.") 
+        }
+    }
+    finally{
+        setLoading(false)
+    }
+  }
+
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </label>
+
+        {error && <p>{error}</p>}
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </main>
+  );
+}
+
+
